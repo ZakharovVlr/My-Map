cb6bc5f0ef2deaab3afe5ddfea8503564f112410
Valery Zakharov <zakharovvalery80@gmail.com>
2026-08-14 22:34:34 +0300

    fix: use asset query url for geojson

--- src/main.js
+++ src/main.js
@@ -1,28 +1,25 @@
 import './style.css'
 import { getBounds, createProjection, renderMap } from './core/Projection.js'
 import { loadMapData } from './data/loadMapData.js';
+import lopatinoUrl from '../public/data/lopatino.geojson?url'; // 1. Импортировали путь
 
-// 1. Добавляем тег в HTML и настраиваем холст СРАЗУ
+// 2. Настраиваем холст
 document.querySelector('#app').innerHTML = `<canvas id="mapCanvas"></canvas>`;
-
 const canvas = document.getElementById('mapCanvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// 2. И только когда холст готов, запускаем асинхронную загрузку карты
-// Используем BASE_URL, чтобы Vite в интернете правильно подставил папку /My-Map/
-const dataUrl = `${import.meta.env.BASE_URL}data/lopatino.geojson`;
-
-loadMapData(dataUrl).then(geojson => {
+// 3. Запускаем загрузку по гарантированному пути
+loadMapData(lopatinoUrl).then(geojson => {
   console.log(geojson);
 
   const bounds = getBounds(geojson);
   const project = createProjection(bounds, canvas.width, canvas.height);
 
-  // Теперь ctx и canvas гарантированно существуют и доступны здесь!
   renderMap(geojson, project, ctx);
 });
+
 
 
